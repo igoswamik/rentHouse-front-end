cf6d7760d2afb0da513ed7088dae72c4b4e991d4
Utkarsh Kumar <utkarsh18e@iitg.ac.in>
2021-07-01 18:20:17 +0530

    login and register page form created

--- src/views/Components/Login.js
+++ src/views/Components/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
+import loginCss from "./Login.css"
 
 function Login() {
-  return <div>Login Page</div>;
+  return (
+  <div>
+    <form className="loginForm">
+      <div className="box">
+        <div className="inputItem">
+          <label>Email: </label> 
+          <input type="email" placeholder="Email"></input>
+        </div>
+        <div className="inputItem">
+          <label>Password:</label>
+          <input type="password" placeholder="Password" required></input>
+        </div>
+      </div>
+      <button>Submit</button>
+    </form>
+  </div>);
 }
 
 export default Login;
--- src/views/Components/Register.js
+++ src/views/Components/Register.js
@@ -1,6 +1,27 @@
 import React from "react";
-function Register() {
-  return <div>Register Page</div>;
-}
 
+import loginCss from "./Login.css"
+
+function Register() {
+  return (
+  <div>
+    <form className="loginForm">
+      <div className="box">
+        <div className="inputItem">
+          <label>Name: </label> 
+          <input type="text" placeholder="Name"></input>
+        </div>
+        <div className="inputItem">
+          <label>Email: </label> 
+          <input type="email" placeholder="Email"></input>
+        </div>
+        <div className="inputItem">
+          <label>Password:</label>
+          <input type="password" placeholder="Password" required></input>
+        </div>
+      </div>
+      <button>Submit</button>
+    </form>
+  </div>);
+}
 export default Register;
